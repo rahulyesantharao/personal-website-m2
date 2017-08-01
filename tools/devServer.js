a9a5047897d2af3e73180275b636cfd6c684c220
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-01 18:28:48 -0500

    Added devServer and webpack dev config

---
+++ tools/devServer.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+const open = require('open');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack/webpack.config.dev');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+let test = 1;
+
+// Configuration for Express app
+const CONFIG = {
+  PORT: 3000, // the port to run the dev server on
+  COMPILER: webpack(webpackConfig) // the webpack compiler instance with dev configurations (provided to webpack-dev-middleware)
+}
+
+// Initialize Express server
+const app = express();
+
+// Use the Webpack Dev Middleware to serve the Webpack content from memory without writing files
+app.use(webpackDevMiddleware(CONFIG.COMPILER, {
+  publicPath: webpackConfig.output.publicPath,
+  stats: { colors: true }
+}));
+
+// For any request, send the src/index.html
+app.get('*', function(req, res) {
+  console.log(test + '. Sending index.html');
+  test++;
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+// Start the server
+app.listen(CONFIG.PORT, function(err) {
+  if(err) {
+    console.log(err);
+  } else { // if there is no error, open the server on CONFIG.PORT
+    open(`http://localhost:${CONFIG.PORT}`);
+  }
+});
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CONFIG = {
+  SRC: path.join(__dirname, '..', 'src'),
+  DIST: path.join(__dirname, '..', 'dist')
+};
+
+module.exports = {
+  entry: [
+    path.join(CONFIG.SRC, 'index.js')
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    path: CONFIG.DIST,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // matches all .js, .jsx files
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      },
+      {
+        test: /\.css$/, // matches all .css files
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/, // matches all .scss files
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg$/, // matches all .svg files
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // limit: 8192,
+              // mimetype: 'image/svg+xml'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  target: "web",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(CONFIG.SRC,'index.html'),
+      inject: true
+    })
+  ]
+};
