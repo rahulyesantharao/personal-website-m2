8f07c1e6613e20185fc04f623117151575373990
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-01 19:41:46 -0500

    Added boilerplate changes to navigation

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,8 +4,10 @@ import {NavLink} from 'react-router-dom';
 const Header = () => (
   <header>
     <nav>
-      <NavLink exact to="/" activeClassName="active"><strong>Home</strong></NavLink>
+      <NavLink exact to="/" activeClassName="active">Rahul Y</NavLink>
+      { " | " }
       <NavLink exact to="/about" activeClassName="active">About</NavLink>
+      { " | " }
       <NavLink exact to="/projects" activeClassName="active">Projects</NavLink>
     </nav>
   </header>
