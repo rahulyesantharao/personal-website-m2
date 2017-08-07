ae9df33265e24d4be5d608ac1398d0687946caab
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-07 12:56:23 -0500

    Aesthetic changes

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectsPage = () => {
   return (
-    <section className="container">
+    <section className="container-fluid">
       <div className="columns">
         <h1 className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop">Projects</h1>
         <div className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop">
