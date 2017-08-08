19ac2d2282503c29bcf854b5f54d3d7cfdd77905
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-08 01:22:53 -0500

    Updated content

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -4,7 +4,7 @@ const ProjectsPage = () => {
   return (
     <section className="container-fluid">
       <div className="columns">
-        <h1 className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop">Projects</h1>
+        <h1 className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop"><span>Projects</span></h1>
         <div className="column is-12-mobile is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop">
           This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.
 
