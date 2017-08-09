31ec1d344bb16dc9f9bc3487a6511bdfc9807960
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-09 01:58:43 -0500

    Made navbar drawer disable site

--- src/components/App.js
+++ src/components/App.js
@@ -27,10 +27,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={this.state.menu?{overflow:"hidden"}:{}}>
         <Header menuOpen={this.state.menu} toggleMenu={this.toggleMenu.bind(this)} isHome={this.state.home}/>
         <Main isHome={this.state.home} setHome={this.setHome.bind(this)} unsetHome={this.unsetHome.bind(this)}/>
         <Footer isHome={this.state.home}/>
+        <div id="sidenav-overlay" className={this.state.menu?"is-active":""} onClick={this.toggleMenu.bind(this)}/>
       </div>
     );
   }
