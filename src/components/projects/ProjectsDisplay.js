a9655ab8554f9c47bc8e9d80f32048ec2bb06687
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-09 12:25:22 -0500

    Changed projects display to two columns for mobile

--- src/components/projects/ProjectsDisplay.js
+++ src/components/projects/ProjectsDisplay.js
@@ -3,38 +3,38 @@ import ProjectsLink from './ProjectsLink';
 
 const ProjectsDisplay = () => {
   return (
-    <div className="columns projectsPage">
-      <div className="column is-12-mobile is-offset-2-tablet is-4-tablet is-offset-3-desktop is-2-desktop">
+    <div className="columns projectsPage is-mobile">
+      <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-offset-3-desktop is-2-desktop">
         <ProjectsLink to="/projects/ZeroRobotics" text="ZeroRobotics 2014 HS Tournament">
           <img src={require('../../images/zerorobotics.jpg')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-4-tablet is-2-desktop">
+      <div className="column is-6-mobile is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/FindR" text="FindR">
           <img src={require('../../images/findr.png')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
+      <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/Recyclo" text="Recyclo">
           <img src={require('../../images/recyclo.png')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-4-tablet is-offset-3-desktop is-2-desktop">
+      <div className="column is-6-mobile is-4-tablet is-offset-3-desktop is-2-desktop">
         <ProjectsLink to="/projects/MoveOver" text="MoveOver">
           <img src={require('../../images/moveover.png')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
+      <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/CLHSOrchestra" text="CLHS Orchestra">
           <img src={require('../../images/clhsorch.png')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-4-tablet is-2-desktop">
+      <div className="column is-6-mobile is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/Polymap" text="Polymap">
           <img src={require('../../images/clhsorch.png')}/>
         </ProjectsLink>
       </div>
-      <div className="column is-12-mobile is-offset-2-tablet is-4-tablet is-offset-3-desktop is-2-desktop">
+      <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-offset-3-desktop is-2-desktop">
         <ProjectsLink to="/projects/PersonalWebsite" text="Personal Website">
           <img src={require('../../images/clhsorch.png')}/>
         </ProjectsLink>
