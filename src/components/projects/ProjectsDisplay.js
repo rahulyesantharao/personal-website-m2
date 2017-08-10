8cca30258851137a72154649e8502aff151acd89
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-08-10 11:53:42 -0500

    Optimized project tile images

--- src/components/projects/ProjectsDisplay.js
+++ src/components/projects/ProjectsDisplay.js
@@ -6,37 +6,37 @@ const ProjectsDisplay = () => {
     <div className="columns projectsPage is-mobile">
       <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-offset-2-desktop is-2-desktop">
         <ProjectsLink to="/projects/ZeroRobotics" text="ZeroRobotics 2014 HS Tournament">
-          <img src={require('../../images/zerorobotics.jpg')}/>
+          <img src={require('../../images/project-tiles/min/zerorobotics.min.jpg')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/FindR" text="FindR">
-          <img src={require('../../images/findr.png')}/>
+          <img src={require('../../images/project-tiles/min/findr.min.png')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/Recyclo" text="Recyclo">
-          <img src={require('../../images/recyclo.png')}/>
+          <img src={require('../../images/project-tiles/min/recyclo.min.png')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/MoveOver" text="MoveOver">
-          <img src={require('../../images/moveover.png')}/>
+          <img src={require('../../images/project-tiles/min/moveover.min.png')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-offset-2-desktop is-2-desktop">
         <ProjectsLink to="/projects/CLHSOrchestra" text="CLHS Orchestra">
-          <img src={require('../../images/clhsorch.png')}/>
+          <img src={require('../../images/project-tiles/min/clhsorch.min.png')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/Polymap" text="Polymap">
-          <img src={require('../../images/clhsorch.png')}/>
+          <img src={require('../../images/project-tiles/min/clhsorch.min.png')}/>
         </ProjectsLink>
       </div>
       <div className="column is-6-mobile is-offset-2-tablet is-4-tablet is-2-desktop">
         <ProjectsLink to="/projects/PersonalWebsite" text="Personal Website">
-          <img src={require('../../images/clhsorch.png')}/>
+          <img src={require('../../images/project-tiles/min/clhsorch.min.png')}/>
         </ProjectsLink>
       </div>
     </div>
