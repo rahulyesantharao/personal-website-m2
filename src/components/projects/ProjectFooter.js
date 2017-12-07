92755677af6f199999b114db04bace783289dfe9
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-12-07 00:54:57 -0500

    Added option to have no 'explore further' link

--- src/components/projects/ProjectFooter.js
+++ src/components/projects/ProjectFooter.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ProjectFooter = (props) => {
+  let hasExplore = (props.type!="none");
   let exploreText = "Check it out!";
   if(props.type==="github") exploreText = "Check out the Github!";
   else if(props.type==="devpost") exploreText = "Check out the Devpost!";
@@ -11,7 +12,7 @@ const ProjectFooter = (props) => {
     <div className="columns is-centered">
       <div className="column is-12-mobile is-8-tablet is-8-desktop is-flex text">
         <p><Link to="/projects">&#8592; Back To Projects</Link></p>
-        <p className="is-right"><a href={props.link} target="_blank" rel="noopener noreferrer">{exploreText}</a></p>
+        {hasExplore && <p className="is-right"><a href={props.link} target="_blank" rel="noopener noreferrer">{exploreText}</a></p>}
       </div>
     </div>
   )
