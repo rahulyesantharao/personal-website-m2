e9b87da4701b8efb6d3d3257a9a30fd8d5361230
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-04-16 03:19:03 -0400

    Fixed blog date on server side

--- src/components/blog/BlogPostList.js
+++ src/components/blog/BlogPostList.js
@@ -3,17 +3,13 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import BlogPostPreview from './BlogPostPreview';
 
-const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 const BlogPostList = (props) => {
   console.log("RENDER BLOGPOSTLIST", props.page); // eslint-disable-line no-console
   return (
       <div className="columns is-centered">
         {props.page.map((el) =>{
-          let date = new Date(el.post_date);
-          let viewDate = MONTH_NAMES[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
           // console.log('Post!'); // eslint-disable-line no-console
-          return (<BlogPostPreview title={el.post_title} content={el.post_html} date={viewDate} id={el.post_id} key={el.post_id} author={el.author.name}/>);
+          return (<BlogPostPreview title={el.post_title} content={el.post_html} date={el.post_date} id={el.post_id} key={el.post_id} author={el.author.name}/>);
         })}
         <div className="column is-12-mobile is-8-tablet is-8-desktop is-flex text">
           {props.hasNewer && <p><Link to={`/blog/pages/${props.pageNum-1}`}>&#8592; Newer Posts</Link></p>}
