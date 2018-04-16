b2ead2e2db7e6886dc345cc33a8b410a4e5ae869
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-04-16 00:28:02 -0400

    Made blog post footer support custom page backlinks

--- src/components/blog/BlogPostFooter.js
+++ src/components/blog/BlogPostFooter.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
-const BlogPostFooter = () => {
+const BlogPostFooter = (props) => {
   return (
     <div className="columns is-centered">
       <div className="column is-12-mobile is-8-tablet is-8-desktop is-flex text">
-        <p><Link to="/blog">&#8592; Back To Blog</Link></p>
+        <p><Link to={`/blog/pages/${props.page}`}>&#8592; Back To Blog</Link></p>
       </div>
     </div>
   )
+};
+BlogPostFooter.propTypes = {
+  page: PropTypes.number.isRequired
 };
 export default BlogPostFooter;
