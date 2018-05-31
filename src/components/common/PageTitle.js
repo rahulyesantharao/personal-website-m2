d7bad50506f5fb220c8207f0badd3c8fcd8e30f2
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-05-31 15:52:16 -0400

    Made PageTitle styling inline

--- src/components/common/PageTitle.js
+++ src/components/common/PageTitle.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const titleStyle = {
+  backgroundSize: 'cover',
+  backgroundPosition: '50% 100%',
+  backgroundAttachment: 'fixed',
+  backgroundRepeat: 'no-repeat',
+  padding: '90px 0'
+};
+
 const PageTitle = (props) => {
   return(
-    <section className="container-fluid" id={props.titleId}>
+    <section className="container-fluid" id={props.titleId} style={titleStyle}>
       <div className="container">
         <div className="columns is-centered">
           <div className="column is-12-mobile is-8-tablet is-8-desktop">
